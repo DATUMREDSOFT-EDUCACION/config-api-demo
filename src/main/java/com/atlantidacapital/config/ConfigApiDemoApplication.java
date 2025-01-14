13a06a024b4f1e6880acc897cb3bf2b7262c90d6
datum-ctrl-app <74498239+datum-ctrl-app@users.noreply.github.com>
2025-01-13 22:39:40 -0500

    Cambios de App
    Cambios de App

---
+++ src/main/java/com/atlantidacapital/config/ConfigApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.atlantidacapital.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlantidacapital/config/ConfigApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlantidacapital.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
